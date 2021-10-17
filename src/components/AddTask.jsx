474620899ad9960288e59a51f0a1151a62369399
Eduardo <galvaocruz16@gmail.com>
2021-10-17 11:33:21 -0300

    versão final

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './AddTask.css'
+import Button from './Button';
+
+
+const AddTask = ({handleTaskAddition}) => {
+    const[inputData, setInputData] =useState('');
+
+    const handleInputChange =(e) =>{
+        setInputData(e.target.value);
+    }
+    const handleAddTaskClick =()=>{
+        handleTaskAddition(inputData)
+        setInputData("");
+    }
+    return ( 
+        <div className="task__container__add">
+            <input 
+            onChange={handleInputChange} 
+            value={inputData} 
+            className="task__container__add--input" 
+            type="text" 
+            />
+            <Button onClick ={handleAddTaskClick}>Adicionar</Button>
+        </div>
+     );
+}
+ 
+export default AddTask ;
---
+++ src/components/Task.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Task.css"
+import { useHistory } from "react-router-dom";
+import { CgClose, CgInfo} from "react-icons/cg";
+
+
+const  Task = ({task, handleTaskClick, handleTaskRemove}) => {
+   const history = useHistory();
+
+   const handleTaskDetailsClick = () =>{
+      history.push(`/${task.title.replace("/", "-")}`)
+   }
+    return (
+      //   <div className ='task__container'>
+      //      {task.title} 
+      //   </div>
+      <div className='task__container' style={task.completed?{borderLeft:"6px solid #ff00a0"}:{}}>
+         <div className="task__container--title" onClick={()=>handleTaskClick(task.id)}>
+                {task.title} 
+         </div>
+         <div className="task__container__buttons">
+            <button onClick={()=>handleTaskRemove(task.id)} className="remove__task"><CgClose/></button>
+            <button className="see__task" onClick={handleTaskDetailsClick}><CgInfo/></button>
+         </div>
+        
+      </div>
+
+     );
+}
+ 
+export default Task;
